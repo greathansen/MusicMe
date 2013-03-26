075f003b306ec9604345582e55b3d7b074bf82ef
gustavo hansen <gustavo.a.hansen@gmail.com>
2013-03-26 16:49:57 -0300

    Firts Commit. Added MusicMe templates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicMe::Application.config.secret_token = '9338e99bf32fc9905f9916ec0b0ff6abe8c909181fb624840cbde59fb080f39edce09e02a76baff83d361e3f169a159327ca20dda52b541abc7442c8dea88c42'
